c8783d5b670f4f6b3d5d6679045d8a6c3d917cf2
Jakub Kapała <jakub@kapala.xyz>
2026-05-31 13:06:02 +0200

    feat: add useRedeemReward

---
+++ hooks/mutations/useRedeemReward.ts
@@ -0,0 +1,28 @@
+"use client";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import apiClient from "@/lib/apiClient";
+import { RedeemRewardResponse } from "@/types/RedeemRewardResponse.type";
+
+export function useRedeemReward() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: [{ resource: "rewards", scope: "redeem" }],
+    mutationFn: async (rewardId: string) => {
+      const { data } = await apiClient.post<RedeemRewardResponse>(
+        "/rewards/redeem",
+        {
+          rewardId,
+        },
+      );
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [{ resource: "user" }] });
+      queryClient.invalidateQueries({
+        queryKey: [{ resource: "transactions" }],
+      });
+    },
+  });
+}
